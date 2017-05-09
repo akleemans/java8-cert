d7bdb06f8871e9b4e69731dbb5f9644c8bc80daa
Adrianus Kleemans <a.kleemans@gmail.com>
2017-05-09 09:30:28 +0200

    Adding Enum example

---
+++ src/ch/diso/javacert/basics/EnumExample.java
@@ -0,0 +1,13 @@
+package ch.diso.javacert.basics;
+
+public enum EnumExample {
+    FOO, BAR;
+
+    public static void main(String[] args) {
+        // will print the string representation of the Enum (= the enum as string)
+        System.out.println(EnumExample.BAR); // BAR
+
+        // it's also possible to print the ordinal
+        System.out.println("Ordinal of BAR:" + EnumExample.BAR.ordinal()); // 1 (FOO = 0, BAR = 1)
+    }
+}
