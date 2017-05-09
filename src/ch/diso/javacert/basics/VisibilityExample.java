da5cccb99ed50e407df20cc9e4d56b1ba1f5bfb8
Adrianus Kleemans <a.kleemans@gmail.com>
2017-05-09 11:35:13 +0200

    Example for visibility & extending classes

---
+++ src/ch/diso/javacert/basics/VisibilityExample.java
@@ -0,0 +1,33 @@
+package ch.diso.javacert.basics;
+
+public class VisibilityExample {
+
+    public static void main(String[] args) {
+        // p has - after the line below - only the members of 'Professional' available,
+        // unless it is explicitly cast to "(Banker) p".
+        Professional p = new Banker();
+        // p.announceSalary(); does not compile - announceSalary() has private access in 'Professional'
+        p.doWork();
+    }
+}
+
+// The class definitions below are only allowed because they're not public,
+// only one public class/interface allowed, "there must be at most one [top level public] type per compilation unit."
+
+abstract class Professional {
+    private void announceSalary() {
+        System.out.println("I earn money!");
+    }
+
+    abstract void doWork();
+}
+
+class Banker extends Professional {
+    public void announceSalary() {
+        System.out.println("I earn alot of money!");
+    }
+
+    public void doWork() {
+        System.out.println("Counting other people's money...");
+    }
+}
